eb1dd8bfae1d9ed9ecfd111c4c0c2da6e72f3500
greemfox <pavelkurchanov.lpk@gmail.com>
2026-07-03 16:46:23 +0700

    factor funcs out, delay payload decoding

--- main.go
+++ main.go
@@ -9,27 +9,14 @@ import (
 	"time"
 )
 
-/*
-What I do know so far:
-    - Listing public events yields one of four responses:
-        - 200 (OK);
-        - 304 (Not modified);
-        - 403 (Forbidden);
-        - 503 (Service unavailable).
-    - The timeline will include up to 300 events.
-    - Events older than 30 days will never be included.
-    - For queries by user, the default params fetch a single page of 30 results.
-        - 100 is the upper limit for per_page
-*/
-
-type Event struct {
-	ID        string    `json:"id"`
-	Type      string    `json:"type"`
-	Actor     Actor     `json:"actor"`
-	Repo      Repo      `json:"repo"`
-	Payload   Payload   `json:"payload"`
-	Public    bool      `json:"public"`
-	CreatedAt time.Time `json:"created_at"`
+type EventEnvelope struct {
+	ID        string          `json:"id"`
+	Type      string          `json:"type"`
+	Actor     Actor           `json:"actor"`
+	Repo      Repo            `json:"repo"`
+	Public    bool            `json:"public"`
+	CreatedAt time.Time       `json:"created_at"`
+	Payload   json.RawMessage `json:"payload"`
 }
 
 type Actor struct {
@@ -47,9 +34,9 @@ type Repo struct {
 	URL  string `json:"url"`
 }
 
-type Payload struct {
+type PushPayload struct {
 	RepositoryID int    `json:"repository_id"`
-	PushID       int64  `json:"push_id"`
+	PushID       int    `json:"push_id"`
 	Ref          string `json:"ref"`
 	Head         string `json:"head"`
 	Before       string `json:"before"`
@@ -59,39 +46,62 @@ func userEventsEndpoint(username string) string {
 	return "https://api.github.com/users/" + username + "/events"
 }
 
+func makeGetRequest(endpoint string) *http.Request {
+	req, err := http.NewRequest("GET", endpoint, nil)
+	if err != nil {
+		fmt.Println("Error forming request:", err)
+		return nil
+	}
+	req.Header.Set("Accept", "application/vnd.github+json")
+	return req
+}
+
+func getResponse(req *http.Request) *http.Response {
+	clt := http.DefaultClient
+	resp, err := clt.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request", err)
+		return nil
+	}
+	fmt.Println("Response status:", resp.Status)
+	return resp
+}
+
+func getRawEvents(resp *http.Response) []byte {
+	defer resp.Body.Close()
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading event data:", err)
+		return nil
+	}
+	return data
+}
+
+func parseEvents(data []byte) []EventEnvelope {
+	var envs []EventEnvelope
+	if err := json.Unmarshal(data, &envs); err != nil {
+		fmt.Println("Error parsing event data:", err)
+		return nil
+	}
+	fmt.Println("Events extracted!")
+	return envs
+}
+
 func main() {
 	args := os.Args[1:]
 	user := args[0]
 
-	client := http.DefaultClient
 	endpoint := userEventsEndpoint(user)
 	fmt.Println("Endpoint:", endpoint)
 
-	req, err := http.NewRequest("GET", endpoint, nil)
-	if err != nil {
-		fmt.Println("Error forming request:", err)
-		return
-	}
-	req.Header.Set("Accept", "application/vnd.github+json")
+	request := makeGetRequest(endpoint)
+	response := getResponse(request)
+	eventData := getRawEvents(response)
+	envelopes := parseEvents(eventData)
+	// Still yet to parse the payloads properly... or at all.
+	// But at least I'm no longer losing data that doesn't fit push events.
 
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Error sending request", err)
-		return
+	for idx, event := range envelopes {
+		fmt.Print("#", idx+1, "\n", event, "\n")
 	}
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Error reading from body:", err)
-		return
-	}
-	fmt.Println("Response status:", resp.Status)
-
-	var events []Event
-	err = json.Unmarshal(body, &events)
-	if err != nil {
-		fmt.Println("Error parsing JSON:", err)
-		return
-	}
-	fmt.Printf("Events extracted!\n\n%v\n\n", events)
 }
